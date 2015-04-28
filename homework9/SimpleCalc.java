2be7847dfe054c2fe539039849c82718fc38a42f
Steff Bisinger <srbcat@gmail.com>
2015-04-28 10:35:13 -0500

    Nearly there! :cat: :boom:

--- homework9/SimpleCalc.java
+++ homework9/SimpleCalc.java
@@ -4,37 +4,93 @@
 *	@description	This is a calculator that uses a state machine style approach.
 */
 
+import java.util.ArrayList<E>;
+import java.lang.Number;
 
 public class SimpleCalc{
 	
-	private String prevState;
 	private String state;
 
 	public SimpleCalc(){
-		state = init;
+		state = "oparen";
 		prevState = NULL;
 	}
 
 	private Number addNums(Number x, Number y){
-		return a+b;
+		Float f = Float(x.floatValue()+y.floatValue());
+		return f;
 	}
 
 	private Number subNums(Number x, Number y){
-		return a-b;
+		Float f = Float(x.floatValue()-y.floatValue()); 
+		return f;
 	}
 
 	private Number multNums(Number x, Number y){
-		return a*b;
+		Float f = Float(x.floatValue()*y.floatValue()); 
+		return f;
 	}
 
 	private Number divNums(Number x, Number y){
-		//TODO: check for dividing by zero and catch it
-		return a/b;
+		if(y.intValue()==0){
+			System.out.println("Error: Cannot divide by zero.");
+			return NULL;
+		}
+		Float f = Float(x.floatValue()/y.floatValue()); 
+		return f;
 	}
 
 	public Number parseEquation(String s){
+		Float answer = NULL;
+		ArrayList<String> equation = new ArrayList<String>(Arrays.asList(s.split("(?!^)"));
+		ArrayList<Float> operands = new ArrayList<Number>();
+		ArrayList<String> operators = new ArrayList<String>();
+		String s = "";
+		int i=0;
 		while(!state.equals("exit"){
-			
+			if(state.equals("oparen"){
+				i=equation.lastIndexOf("(");
+				if(i>=0){
+				operators.add(equation.get(i));
+				equation.remove(i);
+				state = "num";
+				}
+				else if(equation.size()==1){
+					state = "exit";
+				}
+				else{
+					i=0;
+					state = "num";
+				}
+			}
+
+			if(state.equals("num"){
+				if(isDigit(equation.get(i))){
+					s+=equation.get(i);
+					equation.remove(i);
+					i++;
+				}
+
+				if(!isDigit(equation.get(i))){
+					if(equation.get(i).equals(")"))
+						state = "cparen";
+					else
+						state = "operator";
+					Float f = new Float(s);
+					operands.add(f);
+				}
+			}
+
+			if(state.equals("cparen"){
+				
+			}
 		}
+		return equation.get(0);
+	}
+
+	private boolean isDigit(String str){
+		if(str.matches("?\\d^") || str.matches("?\\.^"))
+			return true;
+		return false;
 	}
 }
